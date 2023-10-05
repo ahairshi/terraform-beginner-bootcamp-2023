8eec377e7c2969b56376046484c6df3f862b0533
ahairshi <daisyfuentesahamed@gmail.com>
2023-10-05 18:03:27 +0000

    #23 bucket should no longer use random provider

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 # }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "website_bucket" {
   # Bucket Naming Rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = var.bucket_name
