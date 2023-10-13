a655e5ca0ba727213c85859fd07727767b60e1b8
ahairshi <daisyfuentesahamed@gmail.com>
2023-10-14 02:55:02 +0530

    #48 Fix issues with provider, set up three different towns. (#49)

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,44 @@
-output "bucket_name" {
+output "bucket_name_tennis" {
   description = "Bucket name for our static website hosting"
-  value = module.terrahouse_aws.bucket_name
+  value = module.home_tennis_hosting.bucket_name
 }
 
-output "website_endpoint" {
+output "s3_website_endpoint_tennis" {
   description = "S3 Static Website hosting endpoint"
-  value = module.terrahouse_aws.website_endpoint
+  value = module.home_tennis_hosting.website_endpoint
 }
 
-output "cloudfront_url" {
+output "cloudfront_url_tennis" {
   description = "The CloudFront Distribution Domain Name"
-  value = module.terrahouse_aws.cloudfront_url
+  value = module.home_tennis_hosting.domain_name
+}
+
+output "bucket_name_spotify" {
+  description = "Bucket name for our static website hosting"
+  value = module.home_spotify_hosting.bucket_name
+}
+
+output "s3_website_endpoint_spotify" {
+  description = "S3 Static Website hosting endpoint"
+  value = module.home_spotify_hosting.website_endpoint
+}
+
+output "cloudfront_url_spotify" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.home_spotify_hosting.domain_name
+}
+
+output "bucket_name_movie" {
+  description = "Bucket name for our static website hosting"
+  value = module.home_movie_hosting.bucket_name
+}
+
+output "s3_website_endpoint_movie" {
+  description = "S3 Static Website hosting endpoint"
+  value = module.home_movie_hosting.website_endpoint
+}
+
+output "cloudfront_url_movie" {
+  description = "The CloudFront Distribution Domain Name"
+  value = module.home_movie_hosting.domain_name
 }
